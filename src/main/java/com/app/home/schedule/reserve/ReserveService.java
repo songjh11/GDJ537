202fdabbb296337671da156ff2313b76729d898d
Gumaaa <skc06040@naver.com>
2022-12-07 11:23:39 +0900

    초기 세팅

---
+++ src/main/java/com/app/home/schedule/reserve/ReserveService.java
@@ -0,0 +1,11 @@
+package com.app.home.schedule.reserve;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ReserveService {
+
+}
