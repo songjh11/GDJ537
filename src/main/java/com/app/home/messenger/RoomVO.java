6f3e410e45dd2bd519fb69806dfd4f6a944c8a94
KOOHYO <zpfhzpfh112@naver.com>
2022-12-08 20:43:03 +0900

    채팅방 만드는중

--- src/main/java/com/app/home/messenger/RoomVO.java
+++ src/main/java/com/app/home/messenger/RoomVO.java
@@ -1,17 +1,25 @@
 package com.app.home.messenger;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class RoomVO {
 
 	// 채팅방 번호
-	private int roomNum;
+	private Integer roomNum;
+	// 채팅방 생성일자
+	private Date regDate;
 	// 채팅방 이름
 	private String roomName;
+	// 채팅방 방장
+	private Integer hostId;
+	// 채팅방 비밀번호
+	private Integer pw;
 	
-	public String toString() {
-		return "Room[roomNum="+roomNum+", roomName="+roomName+"]";
-	}
+//	public String toString() {
+//		return "Room[roomNum="+roomNum+", roomName="+roomName+"]";
+//	}
 	
 }
