bb132032c8fe96b222423e006713f2861aed6582
parkvsmin <sdfjkl658@naver.com>
2022-12-09 15:50:21 +0900

    익명댓글

--- src/main/java/com/app/home/file/FileDAO.java
+++ src/main/java/com/app/home/file/FileDAO.java
@@ -4,8 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FileDAO {
-	
+
 	public int setFile(FileVO fileVO)throws Exception;
+
 	public FileVO getFileDetail(FileVO fileVO)throws Exception;
+
 	public int setFileDelete(FileVO fileVO)throws Exception;
 }
