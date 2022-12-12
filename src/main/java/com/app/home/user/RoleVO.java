2f00f073e5579548e768c8e8fe3b3e2b11290b6a
vpfl95 <k_dh610@naver.com>
2022-12-12 14:36:24 +0900

    d

--- src/main/java/com/app/home/user/RoleVO.java
+++ src/main/java/com/app/home/user/RoleVO.java
@@ -7,4 +7,12 @@ public class RoleVO {
 	
 	private int roleNum;
 	private String roleName;
+	
+//	public String getRoleName() {
+//		this.roleName = "ROLE_" + this.getRoleName();
+//		return this.roleName;
+//	}
 }
+
+
+
