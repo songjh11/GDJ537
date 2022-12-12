704c2dc0767228b2a3d97a4704cfbef0ce32adf3
daeki <kimdaeki@gmail.com>
2022-12-12 11:10:04 +0900

    WebConfig Update

--- src/main/java/com/app/home/util/WebConfig.java
+++ src/main/java/com/app/home/util/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-@Configuration
+//@Configuration
 public class WebConfig {
 	
 	@Bean
