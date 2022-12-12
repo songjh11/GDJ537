5e723a3b75462d29f02c39ce09917ba77de961bb
daeki <kimdaeki@gmail.com>
2022-12-12 11:10:04 +0900

    WebConfig Update

--- src/main/java/com/app/home/config/WebConfig.java
+++ src/main/java/com/app/home/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.app.home.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
@@ -25,5 +27,10 @@ public class WebConfig implements WebMvcConfigurer{
 				
 		
 	}
+	
+	@Bean
+    MappingJackson2JsonView jsonView(){
+        return new MappingJackson2JsonView();
+    }
 	
 }
