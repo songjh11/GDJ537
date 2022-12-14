f69a9c0a820911fabb087d4a116210506fe4c39b
seok2609 <hjs120926@naver.com>
2022-12-14 15:25:28 +0900

    123213211

--- src/main/java/com/app/home/report/vaca/ReportVacaVO.java
+++ src/main/java/com/app/home/report/vaca/ReportVacaVO.java
@@ -52,7 +52,7 @@ public class ReportVacaVO extends ReportApplyVO {
 	public Long getCall() {
 		return call;
 	}
-	public void setCall(Integer call) {
+	public void setCall(Long call) {
 		this.call = call;
 	}
 	public Date getDate() {
@@ -73,18 +73,7 @@ public class ReportVacaVO extends ReportApplyVO {
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
-	public int getDepNum() {
-		return depNum;
-	}
-	public void setDepNum(int depNum) {
-		this.depNum = depNum;
-	}
-	public UserVO getUserVO() {
-		return userVO;
-	}
-	public void setUserVO(UserVO userVO) {
-		this.userVO = userVO;
-	}
+
 	
 	
 	
--- src/main/resources/static/js/hjs/vacation.js
+++ src/main/resources/static/js/hjs/vacation.js
@@ -26,40 +26,40 @@ const d3 = document.querySelector("#d3");   // 신청일의 일
 //자바스크립트 에서는 date형식을 사용할때 0월~11월과 같은형식이므로
 // +1을 해줘야하지만 number타입으로 사용해서 필요없음
 
-// 시작 년/월/일의 밸류
-let yv1 = y1.value;
-let mv1 = m1.value;
-let dv1 = d1.value;
+// // 시작 년/월/일의 밸류
+// let yv1 = y1.value;
+// let mv1 = m1.value;
+// let dv1 = d1.value;
 
-// 끝 년/월/일의 밸류
-let yv2 = y2.value;
-let mv2 = m2.value;
-let dv2 = d2.value;
+// // 끝 년/월/일의 밸류
+// let yv2 = y2.value;
+// let mv2 = m2.value;
+// let dv2 = d2.value;
 
-// 신청일의 숫자들을 date형식으로 바꿔서 디비에 넣어보자
-let yv3 = y3.value;
-let mv3 = m3.value;
-let dv3 = d3.value;
+// // 신청일의 숫자들을 date형식으로 바꿔서 디비에 넣어보자
+// let yv3 = y3.value;
+// let mv3 = m3.value;
+// let dv3 = d3.value;
 
-// s = s.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+// // s = s.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
 
-// 시작 년/월/일의 합 (숫자형식)
-let bef = yv1 + mv1 + dv1;
+// // 시작 년/월/일의 합 (숫자형식)
+// let bef = yv1 + mv1 + dv1;
 
-// 끝 년/월/일의 합 (숫자형식)
-let aft = yv2 + mv2 + dv2;
+// // 끝 년/월/일의 합 (숫자형식)
+// let aft = yv2 + mv2 + dv2;
 
-// 신청일의 년/월/일을 합침
-let sumD = yv3 + mv3 + dv3;
+// // 신청일의 년/월/일을 합침
+// let sumD = yv3 + mv3 + dv3;
 
-console.log("과연? " , sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
+// console.log("과연? " , sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
 
 
-console.log("1번째 연 월 일", y1.value + m1.value + d1.value);
+// console.log("1번째 연 월 일", y1.value + m1.value + d1.value);
 
-console.log("숫자일까? ", Number(bef))
+// console.log("숫자일까? ", Number(bef))
 
-console.log("뺄셈 : ", Number(aft) - Number(bef));
+// console.log("뺄셈 : ", Number(aft) - Number(bef));
 
 
 $(".mail").click(function(){
@@ -104,11 +104,11 @@ $("#callInput").blur(function(){
 
 //선택한것이나 쓴것을 밸류에 담기
 
-$("#dayGap").val((Number(aft) - Number(bef))+'일');
-console.log($("#dayGap").val());
+// $("#dayGap").val((Number(aft) - Number(bef))+'일');
+// console.log($("#dayGap").val());
 
-$("#dateParse").val(sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
-console.log("dateParse : ", sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
+// $("#dateParse").val(sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
+// console.log("dateParse : ", sumD.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'));
 
 
 // $("#vcaBtn").click(function(){
