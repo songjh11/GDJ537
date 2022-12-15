1d0356ca01d75f61e2f780140c2fb5aec851848b
Gumaaa <skc06040@naver.com>
2022-12-15 16:39:44 +0900

    fix

--- src/main/java/com/app/home/user/config/WebUserConfig.java
+++ src/main/java/com/app/home/user/config/WebUserConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+// @Configuration
 public class WebUserConfig implements WebMvcConfigurer {
    
    @Value("${app.upload.base}")
