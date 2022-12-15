38e12a0208dd127e4f221bf61cac60b85138468a
KOOHYO <zpfhzpfh112@naver.com>
2022-12-15 11:25:09 +0900

    채팅방 만드는 것 검증중

--- src/main/java/com/app/home/messenger/MessengerController.java
+++ src/main/java/com/app/home/messenger/MessengerController.java
@@ -59,9 +59,7 @@ public class MessengerController {
 		// ------------------ 채팅목록 ------------------
 		List<RoomVO> roomVOs = new ArrayList<>();
 		RoomVO roomVO = new RoomVO();
-		UserVO userVO2 = new UserVO();
 		userVO.setId(userVO.getId());
-		roomVO.setUserVO(userVO);
 		
 		roomVOs = messengerService.getRoomList(roomVO);
 		
@@ -308,12 +306,19 @@ public class MessengerController {
 	    userVO  = (UserVO)authentication.getPrincipal();
 	    userVO = userService.getMypage(userVO);
 	    
+	    int [] id = roomVO.getId();
+	    
+	    for(int ids : id) {
+	    	log.info("아이디에 뭐가있나? =====> {} ", id);
+	    }
+	    
 	    // 로그인 회원을 방장으로
 		roomVO.setHostId(userVO.getId());
-		UserVO userVO2 = new UserVO();
-		// 방장도 유저이기 때문에 넣어줌
-		userVO2.setId(userVO.getId());
-		roomVO.setUserVO(userVO2);
+//		UserVO userVO2 = new UserVO();
+//		log.info("유저야아아 => {} ", userVO.getId());
+//		// 방장도 유저이기 때문에 넣어줌
+//		userVO2.setId(userVO.getId());
+//		roomVO.setUserVO(userVO2);
 		int result = messengerService.setAddRoom(roomVO);
 		
 		if(result > 0 ) {
