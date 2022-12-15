8715f3b127f361e658bb7956603d59ee5bae70af
kdy991101 <kdy991101@naver.com>
2022-12-15 17:47:55 +0900

    끝

--- src/main/resources/static/js/kdy/vacation.js
+++ src/main/resources/static/js/kdy/vacation.js
@@ -8,23 +8,19 @@ function nullCheck(data){
     }
 }
 
-let caca = false;
+let cateCheck = document.getElementsByClassName("mail")
 
 function vacaNullCheck(){
-    let s1 = document.getElementById("s1");
-    let s2 = document.getElementById("s2");
-    let s3 = document.getElementById("s3");
-    let s4 = document.getElementById("s4");
-    let s5 = document.getElementById("s5");
-    if(s1||s2||s3||s4||s5){
-        caca = true
-    }else{
-        caca = false;
+ let caCh = false;
+    for(let i=0; i<cateCheck.length; i++){
+        if(cateCheck[i].checked == true){
+            caCh = true;
+        }
     }
     let result2 = nullCheck($("#sasa").val())
     let result3 = nullCheck($("#start").val())
     let result4 = nullCheck($("#stop").val())
-    if(caca&&result2&&result3&&result4){
+    if(caCh&&result2&&result3&&result4){
         frm.submit();
     }else{
         alert("종류, 사유, 기간을 확인해주세요!");
