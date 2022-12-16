78e20f9e7cbb23df614ae0988c21ac0e69e39c04
5sbread <oohogong@gmail.com>
2022-12-16 09:46:22 +0900

    size

--- src/main/resources/static/js/messenger/group_chatroom.js
+++ src/main/resources/static/js/messenger/group_chatroom.js
@@ -1,4 +1,24 @@
 
+	let popupWidth = "750";
+	let popupHeight = "550";
+	let popUp = "";
+	  
+	// 듀얼 모니터 고려한 윈도우 띄우기
+	let curX = window.screenLeft;
+	let curWidth = document.body.clientWidth;
+	let curHeight = document.body.clientHeight;
+	  
+	let nLeft = (screen.availWidth/ 2) - (popupWidth / 2);
+	let nTop = ((screen.availHeight-popupHeight)/2)-10;
+	
+	let strOption = "";
+	strOption += "left=" + nLeft + "px,";
+	strOption += "top=" + nTop + "px,";
+	strOption += "width=" + popupWidth + "px,";
+	strOption += "height=" + popupHeight + "px,";
+	strOption += "toolbar=no,menubar=no,location=no,";
+	strOption += "resizable=yes,status=yes";
+
 	function chatPop(roomNum){
 		let win = window.open("./chatroom?roomNum=" +roomNum, "단체 채팅방", strOption);	
 		win.focus();
