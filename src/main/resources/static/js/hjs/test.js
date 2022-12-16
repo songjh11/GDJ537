f373a1e2a1d0ecc1c61a3bfe54b571acea8f1614
seok2609 <hjs120926@naver.com>
2022-12-17 00:34:27 +0900

    각 lstatus, roleNum마다 보이는 페이지

--- src/main/resources/static/js/hjs/test.js
+++ src/main/resources/static/js/hjs/test.js
@@ -13,19 +13,28 @@ console.log("attr:: " , $("#dpName").attr("data-hh"));
 $("#dpName").ready(function(event){
 
     console.log("h6텍스트 바꾸자");
-    console.log("디스 : " , $(this));
-    console.log("222 : " , $("#dpName").val());
-    console.log("333: " , $("#dpName").text());
-    console.log("222 : " , $(this).val());
+    // console.log("디스 : " , $(this));
+    // console.log("222 : " , $("#dpName").val());
+    // console.log("333: " , $("#dpName").text());
+    // console.log("222 : " , $(this).val());
 
     let dh = $("#aaa").attr("data-hh");
     console.log("attr:: " , $(this).attr("data-hh"));
-    console.log(dh);
+    console.log("dh --> ", dh);
 
     // console.log("333: " , $(this).text());
 
-    if($(dh)){
-
+    if(dh==1){
+        console.log("확인");
+        $("#dpName").text('영업팀');
+    }else if($(dh)==2){
+        $("#dpName").text('총무팀');
+    }else if($(dh)==3){
+        $("#dpName").text('인사팀');
+    }else if($(dh)==4){
+        $("#dpName").text('IT개발팀');
+    }else if(dh==5){
+        $("#dpName").text('생산팀');
     }
 
 })
